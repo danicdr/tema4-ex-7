9a032a89d43c0c218b1a342fcede2622f423fa57
Daniel Codreanu <danielcdr2006@gmail.com>
2023-05-24 21:21:41 +0300

    Add project files.

---
+++ tema4 ex 7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        PrintPiramid(n);
+    }
+
+    static void PrintPiramid(int n)
+    {
+        if (n <= 0)
+            return;
+
+        PrintPiramid(n - 1);
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(n + " ");
+        }
+        Console.WriteLine();
+
+        PrintPiramid(n - 1);
+    }
+}
+
+//Scrieti o functie recursiva care pentru un numar “n” primit ca parametru, va
+//afisa urmatoarea piramida a numerelor:
+
